4cbf30ee00da77038d2dee4cd938121415401257
molly <mollynzembi@gmail.com>
2024-06-03 19:24:07 +0300

    assed my tests

---
+++ lib/insert.sql
@@ -0,0 +1,38 @@
+-- Inserting bears into the 'bears' table
+
+-- 1. Mr. Chocolate
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 8, 'M', 'Brown', 'friendly', 1);
+
+-- 2. Rowdy
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 6, 'M', 'Black', 'playful', 1);
+
+-- 3. Tabitha
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 4, 'F', 'Gray', 'calm', 1);
+
+-- 4. Sergeant Brown
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 10, 'M', 'Brown', 'serious', 1);
+
+-- 5. Melissa
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 7, 'F', 'White', 'gentle', 1);
+
+-- 6. Grinch
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 5, 'M', 'Green', 'grumpy', 1);
+
+-- 7. Wendy
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 3, 'F', 'Brown', 'playful', 1);
+
+-- 8. unnamed
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
+
+
+
+
+
